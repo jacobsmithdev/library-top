4de742c794f159fa4f2f6c6ee857c1cbe2d63220
Jacob Smith <129335957+jsmith-code@users.noreply.github.com>
2025-01-09 09:03:25 -0700

    Add constructor for book object

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.info = function() {
+    let status = this.read ? `read` : `not read`;
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${status}`;
+}
