17696204abefcfa12330bee7bf908f0eb560303d
Jacob Smith <129335957+jsmith-code@users.noreply.github.com>
2025-01-09 11:32:42 -0700

    Add function to append books to library

--- scripts.js
+++ scripts.js
@@ -11,3 +11,8 @@ Book.prototype.info = function() {
     let status = this.read ? `read` : `not read`;
     return `${this.title} by ${this.author}, ${this.pages} pages, ${status}`;
 }
+
+function appendToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    library.push(book);
+}
