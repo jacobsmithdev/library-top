ccd84de40e276a9a5a2c880d9a3258bbdf3a3a88
Jacob Smith <129335957+jsmith-code@users.noreply.github.com>
2025-01-12 14:52:41 -0700

    Add table display for library
    A single 'updateLibraryDisplay' function is used to handle displaying
    data since it means after modifying the library array, calling
    updateLibraryDisplay ensures the html exactly matches it.
    
    Otherwise, parity between html and array is more complicated. After
    modifying the array, you must find the relevant html and modify it in
    the same way, effectively doubling your code for every modification.
    
    While re-generating large sets of data is inefficient, this project is
    so small the performance impact is not a concern and the simpler code is
    worth the tradeoff.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const libraryDisplay = document.querySelector('#library-display');
+
 const library = [];
 
 function Book(title, author, pages, read) {
@@ -17,6 +19,30 @@ function appendToLibrary(title, author, pages, read) {
     library.push(book);
 }
 
+function updateLibraryDisplay() {
+    // Remove previous display elements
+    libraryDisplay.innerText = '';
+
+    library.forEach(book => {
+        const tr = document.createElement('tr');
+        
+        const title = document.createElement('td');
+        const author = document.createElement('td');
+        const pages = document.createElement('td');
+        const read = document.createElement('td');
+
+        title.innerText = book.title;
+        author.innerText = book.author;
+        pages.innerText = book.pages;
+        read.innerText = book.read ? 'Read' : 'Unread';
+
+        tr.append(title, author, pages, read);
+        libraryDisplay.append(tr);
+    })
+}
+
 appendToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
 appendToLibrary('Crime and Punishment', 'Fyodor Dostoevsky', 608, true);
 appendToLibrary('Great Expectations', 'Charles Dickens', 544, false);
+
+updateLibraryDisplay();
