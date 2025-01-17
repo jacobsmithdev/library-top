77f8b20ea65de4848eadb7e9c351af70b682d6a2
Jacob Smith <129335957+jsmith-code@users.noreply.github.com>
2025-01-17 09:32:36 -0700

    Remove unnecessary method on Book
    Function is never called.

--- scripts.js
+++ scripts.js
@@ -12,11 +12,6 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-Book.prototype.info = function() {
-    let status = this.read ? `read` : `not read`;
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${status}`;
-}
-
 function appendToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     library.push(book);
